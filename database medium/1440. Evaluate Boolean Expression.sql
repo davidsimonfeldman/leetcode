d493da0784ffb6890ab51a72f975d1b02aa7c263
David <davidsimonfeldman@gmail.com>
2021-04-22 16:51:31 +0100

    Add files via upload

---
+++ database medium/1440. Evaluate Boolean Expression.sql
@@ -0,0 +1,97 @@
+/*
+1440. Evaluate Boolean Expression
+Medium
+SQL Schema
+Table Variables:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| name          | varchar |
+| value         | int     |
++---------------+---------+
+name is the primary key for this table.
+This table contains the stored variables and their values.
+ 
+
+Table Expressions:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| left_operand  | varchar |
+| operator      | enum    |
+| right_operand | varchar |
++---------------+---------+
+(left_operand, operator, right_operand) is the primary key for this table.
+This table contains a boolean expression that should be evaluated.
+operator is an enum that takes one of the values ('<', '>', '=')
+The values of left_operand and right_operand are guaranteed to be in the Variables table.
+ 
+
+Write an SQL query to evaluate the boolean expressions in Expressions table.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Variables table:
++------+-------+
+| name | value |
++------+-------+
+| x    | 66    |
+| y    | 77    |
++------+-------+
+
+Expressions table:
++--------------+----------+---------------+
+| left_operand | operator | right_operand |
++--------------+----------+---------------+
+| x            | >        | y             |
+| x            | <        | y             |
+| x            | =        | y             |
+| y            | >        | x             |
+| y            | <        | x             |
+| x            | =        | x             |
++--------------+----------+---------------+
+
+Result table:
++--------------+----------+---------------+-------+
+| left_operand | operator | right_operand | value |
++--------------+----------+---------------+-------+
+| x            | >        | y             | false |
+| x            | <        | y             | true  |
+| x            | =        | y             | false |
+| y            | >        | x             | true  |
+| y            | <        | x             | false |
+| x            | =        | x             | true  |
++--------------+----------+---------------+-------+
+As shown, you need find the value of each boolean exprssion in the table using the variables table.
+
+
+SELECT e.left_operand, e.operator, e.right_operand,
+       CASE WHEN operator = '>' THEN IF(v1.value > v2.value, 'true', 'false')
+            WHEN operator = '<' THEN IF(v1.value < v2.value, 'true', 'false')
+            ELSE IF(v1.value = v2.value, 'true', 'false')
+       END AS value
+FROM Expressions e
+JOIN Variables v1 ON v1.name = e.left_operand
+JOIN Variables v2 ON v2.name = e.right_operand;
+
+
+
+
+SELECT e.left_operand, e.operator, e.right_operand,
+    (
+        CASE
+            WHEN e.operator = '<' AND v1.value < v2.value THEN 'true'
+            WHEN e.operator = '=' AND v1.value = v2.value THEN 'true'
+            WHEN e.operator = '>' AND v1.value > v2.value THEN 'true'
+            ELSE 'false'
+        END
+    ) AS value
+FROM Expressions e
+JOIN Variables v1
+ON e.left_operand = v1.name
+JOIN Variables v2
+ON e.right_operand = v2.name
