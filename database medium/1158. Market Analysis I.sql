c3a7747b654235c737c69e4da620f8142c60a087
David <davidsimonfeldman@gmail.com>
2021-04-24 11:57:18 +0100

    Add files via upload

---
+++ database medium/1158. Market Analysis I.sql
@@ -0,0 +1,122 @@
+/*
+1158. Market Analysis I
+Medium
+SQL Schema
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| join_date      | date    |
+| favorite_brand | varchar |
++----------------+---------+
+user_id is the primary key of this table.
+This table has the info of the users of an online shopping website where users can sell and buy items.
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| order_date    | date    |
+| item_id       | int     |
+| buyer_id      | int     |
+| seller_id     | int     |
++---------------+---------+
+order_id is the primary key of this table.
+item_id is a foreign key to the Items table.
+buyer_id and seller_id are foreign keys to the Users table.
+Table: Items
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| item_id       | int     |
+| item_brand    | varchar |
++---------------+---------+
+item_id is the primary key of this table.
+ 
+
+Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.
+
+The query result format is in the following example:
+
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2018-01-01 | Lenovo         |
+| 2       | 2018-02-09 | Samsung        |
+| 3       | 2018-01-19 | LG             |
+| 4       | 2018-05-21 | HP             |
++---------+------------+----------------+
+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2018-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2018-08-04 | 1       | 4        | 2         |
+| 5        | 2018-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+
+Items table:
++---------+------------+
+| item_id | item_brand |
++---------+------------+
+| 1       | Samsung    |
+| 2       | Lenovo     |
+| 3       | LG         |
+| 4       | HP         |
++---------+------------+
+
+Result table:
++-----------+------------+----------------+
+| buyer_id  | join_date  | orders_in_2019 |
++-----------+------------+----------------+
+| 1         | 2018-01-01 | 1              |
+| 2         | 2018-02-09 | 2              |
+| 3         | 2018-01-19 | 0              |
+| 4         | 2018-05-21 | 0              |
+
+
+
+
+
+
+SELECT u.user_id AS buyer_id, join_date, 
+IFNULL(COUNT(order_date), 0) AS orders_in_2019 
+FROM Users as u
+LEFT JOIN Orders as o
+ON u.user_id = o.buyer_id
+AND YEAR(order_date) = '2019'
+GROUP BY u.user_id
+
+
+
+
+ select U.user_id as buyer_id, U.join_date ,
+ count(item_id) as orders_in_2019
+from Users U 
+left join (select order_date, item_id, buyer_id from Orders
+where order_date between '2019-01-01' and '2020-01-01') O
+on U.user_id = O.buyer_id
+group by U.user_id
+
+
+
+
+WITH new AS (SELECT buyer_id, COUNT(order_id) AS orders_tot
+             FROM Orders
+             WHERE YEAR(order_date)=2019
+             GROUP BY buyer_id)
+SELECT U.user_id AS buyer_id, U.join_date , IFNULL(orders_tot,0) AS orders_in_2019
+FROM Users AS U
+LEFT JOIN new
+ON U.user_id=new.buyer_id
+
+
